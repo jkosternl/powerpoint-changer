bf09c421d1c8d28a7aa149b61a5989243682dee8
J. Koster <j.koster@gmx.com>
2019-12-23 20:50:31 +0100

    Process files in sub directory. Conditionally resize text boxes. Move pictures a bit to the left too.

--- src/main/java/org/jacobjob/powerpoint/ManipulatePresentation.java
+++ src/main/java/org/jacobjob/powerpoint/ManipulatePresentation.java
@@ -1,24 +1,26 @@
 package org.jacobjob.powerpoint;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.apache.poi.sl.usermodel.TextShape;
 import org.apache.poi.xslf.usermodel.*;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
+@Log4j2
 public class ManipulatePresentation {
 
-    private static final Logger log = LogManager.getLogger(ManipulatePresentation.class.getName());
+    public static final double newXPosition = 304.2324409448819d;
+    public static final double newYPosition = 14.853543307086614d;
 
-    public void processPowerpointFile(String powerPointFile) throws IOException {
+    public void processPowerpointFile(File powerPointFile) throws IOException {
         XMLSlideShow ppt = new XMLSlideShow(new FileInputStream(powerPointFile));
-        log.info("Slides found: {}", ppt.getSlides().size());
+        log.info("Starting with file: {} - Slides found: {}", powerPointFile.getAbsolutePath(), ppt.getSlides().size());
 
         changePowerPointResolution(ppt);
 
@@ -26,7 +28,7 @@ public class ManipulatePresentation {
             manipulateSlide(slide);
         }
 
-        storeSlideshow(ppt, "output.pptx");
+//        storeSlideshow(ppt, "output.pptx");
     }
 
     private void storeSlideshow(XMLSlideShow ppt, final String filename) throws IOException {
@@ -37,18 +39,22 @@ public class ManipulatePresentation {
     }
 
     private void manipulateSlide(XSLFSlide slide) {
-        log.info(slide);
         log.info("Shapes found: {}", slide.getShapes().size());
         int shapeCounter = 0;
         final ArrayList<XSLFShape> removeList = new ArrayList<>();
         for (final XSLFShape shape : slide.getShapes()) {
             shapeCounter++;
             log.info("Shape {} = name: {}", shapeCounter, shape.getShapeName());
+            if (shapeCounter > 2) log.warn("Shape = {} - {}", shapeCounter, shape);
             if (shape instanceof XSLFTextShape) {
                 XSLFTextShape textShape = (XSLFTextShape) shape;
-                log.info("Contains text: {}", textShape.getText().substring(0, 40));
                 changeFontSize(textShape);
                 changeTextboxSize(textShape);
+            }
+            if (shape instanceof XSLFPictureShape) {
+                XSLFPictureShape pictureShape = (XSLFPictureShape) shape;
+                changePictureLocation(pictureShape);
+                changePictureSize(pictureShape);
             }
             if (shape instanceof XSLFGroupShape) {
                 XSLFGroupShape groupShape = (XSLFGroupShape) shape;
@@ -63,16 +69,42 @@ public class ManipulatePresentation {
 
     private void changePowerPointResolution(final XMLSlideShow ppt) {
         Dimension pgsize = ppt.getPageSize();
-        log.info("Page size = width: {}, height: {}", pgsize.width, pgsize.height); //should be: width: 960, height: 540
+        log.debug("Page size = width: {}, height: {}", pgsize.width, pgsize.height); //should be: width: 960, height: 540
         if (pgsize.width != 960) {
             ppt.setPageSize(new Dimension(960, pgsize.height));
-            log.info("Changed size to 960 pixels width.");
+            log.debug("Changed size to 960 pixels width.");
         }
     }
 
+    private void changePictureSize(XSLFPictureShape pictureShape) {
+        final Rectangle2D anchor = pictureShape.getAnchor();
+        final double ratio = anchor.getHeight() / anchor.getWidth();
+    }
+
+    private void changePictureLocation(XSLFPictureShape pictureShape) {
+        final Rectangle2D anchor = pictureShape.getAnchor();
+        // If this is a full screen picture, do not move it to the right.
+        if (anchor.getX() < newXPosition) {
+            return;
+        }
+        double height = anchor.getHeight();
+        double width = anchor.getWidth();
+        anchor.setFrame(newXPosition, anchor.getY(), height, width);
+        pictureShape.setAnchor(anchor);
+        log.info("Picture adjusted");
+    }
+
     private void changeTextboxSize(final XSLFTextShape textShape) {
+        //Skip only titles
+        int textLength = textShape.getText().length();
         final Rectangle2D anchor = textShape.getAnchor();
-        anchor.setFrame(304.2324409448819d, 14.853543307086614d, 655.7675590551181d, 540d - 20d);
+        if (textLength < 40) {
+            anchor.setFrame(newXPosition, anchor.getY(), anchor.getHeight(), anchor.getWidth());
+            log.info("Changed ONLY frame position - likely title.");
+        } else {
+            anchor.setFrame(newXPosition, newYPosition, 655.7675590551181d, 540d - 20d);
+            log.info("Changed frame size & position");
+        }
         textShape.setAnchor(anchor);
     }
 
--- src/main/java/org/jacobjob/powerpoint/PowerPointChanger.java
+++ src/main/java/org/jacobjob/powerpoint/PowerPointChanger.java
@@ -1,28 +1,51 @@
 package org.jacobjob.powerpoint;
 
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.time.StopWatch;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+@Log4j2
 public class PowerPointChanger {
 
     private static final String POWERPOINT_FILE = "Opw 42.pptx";
     private static final String POWERPOINT_FILE2 = "Opw 585.pptx";
 
-    private static final Logger log = LogManager.getLogger(PowerPointChanger.class.getName());
+//    private static final Logger log = LogManager.getLogger(PowerPointChanger.class.getName());
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         log.info("Starting");
         memStats();
 
+        List<File> fileList = getFileListFromPath(new File("D:/Temp/Databank"));
+        log.info("Found {}", fileList.size());
+
         ManipulatePresentation manipulator = new ManipulatePresentation();
-        manipulator.processPowerpointFile(POWERPOINT_FILE);
+        int failedCount = 0, goodCount = 0;
+        for (File file : fileList) {
+            try {
+                log.info("Stats: Good: {}. Failed: {}", goodCount, failedCount);
+                manipulator.processPowerpointFile(file);
+                goodCount++;
+            } catch (Exception e){
+                log.error("Failed processing: {}", file.getName());
+                failedCount++;
+            }
+        }
+//        manipulator.processPowerpointFile(new File(POWERPOINT_FILE));
 
         memStats();
         stopWatch.stop();
         log.info("Programma beeindigd na: {}", stopWatch);
+    }
+
+    private static List<File> getFileListFromPath(final File directory) {
+        return (LinkedList<File>) FileUtils.listFiles(directory, new String[]{"pptx"}, true);
     }
 
 
